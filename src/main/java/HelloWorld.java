84ffeba0180eb5e055693801f0185d1c4e422b0a
HuyNguyen <nguyendinhquochuy0311@gmail.com>
2021-10-27 15:41:58 +0700

    fix String

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
+        String b = "gasdf";
         int a = 1;
         System.out.println(a);
         System.out.println("");

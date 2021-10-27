af120e263b6435aaf3826fd06dedc0b35d4ab7ae
HuyNguyen <nguyendinhquochuy0311@gmail.com>
2021-10-27 15:45:48 +0700

    fix double

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-
+        double c = 32.13;
+        System.out.println(c);
         String b = "gasdf";
         int a = 1;
         System.out.println(a);

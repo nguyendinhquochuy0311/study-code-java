aa4a00c297bfe3c1690301793a7933e9ad4bd81f
HuyNguyen <nguyendinhquochuy0311@gmail.com>
2021-10-27 15:36:20 +0700

    demo stash

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
+
+        int a = 1;
+        System.out.println(a);
         System.out.println("");
         System.out.println("Hello world");
     }

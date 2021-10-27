25d30262e5cea7565e1360d3d997ef98f562ca72
HuyNguyen <nguyendinhquochuy0311@gmail.com>
2021-10-27 15:52:03 +0700

    update

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,16 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
         System.out.println("asdfadg");
-=======
+
         System.out.println("fadfasdgadgasdfasd");
         System.out.println("gsadfasdg");
->>>>>>> Stashed changes
-=======
+
         System.out.println("fadfasdgadgasdfasd");
         System.out.println("gsadfasdg");
->>>>>>> Stashed changes
         double c = 32.13;
         System.out.println(c);
         String b = "gasdf";
